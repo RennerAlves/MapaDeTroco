fa9ba7e3efce9a0d44e312f7703ad610d26d4777
Renner Alves Martins <renneralves29@gmail.com>
2023-11-27 14:08:44 -0300

    Removendo Construir Troco e melhorando CalcularTroco

--- MapaDeTroco-2Tentativa/Program.cs
+++ MapaDeTroco-2Tentativa/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Globalization;
+using System.Runtime.Intrinsics.Arm;
 using MapaDeTroco;
 
 namespace TrocoApp
@@ -12,8 +15,9 @@ namespace TrocoApp
             List<double> moedasInteiras = new List<double> { 100, 50, 20, 10, 5, 2, 1 };
             List<double> moedasDecimais = new List<double> { 0.50, 0.25, 0.10, 0.05 };
 
+
             //Sem a moeda de 1 centavo, valores como 1135.48 podem não ser resolvíveis.
-            
+
 
             decimal input = 0;
 
@@ -57,6 +61,7 @@ namespace TrocoApp
         static List<TrocoMoeda> CalcularTrocoDinamicoNotas(List<double> moedas, int valor)
         {
             int[] dp = new int[valor + 1];
+            List<TrocoMoeda> troco = new List<TrocoMoeda>();
 
             for (int i = 1; i <= valor; i++)
             {
@@ -69,103 +74,154 @@ namespace TrocoApp
                         dp[i] = Math.Min(dp[i], 1 + dp[i - moedaInt]);
                     }
                 }
+
+
+                if (dp[dp.Length - 1] != 0)
+                {
+
+
+                    int valorAlvo = valor;
+                    int numeroNotas = dp[dp.Length - 1];
+
+
+                    for (int j = 0; j < moedas.Count; j++)
+                    {
+                        int moedaAtual = (int)moedas[j];
+                        int quantidade = Math.Min(numeroNotas, valorAlvo / moedaAtual);
+
+                        if (quantidade > 0)
+                        {
+                            troco.Add(new TrocoMoeda(moedaAtual, quantidade));
+                        }
+
+                        valorAlvo -= quantidade * moedaAtual;
+
+                        if (valorAlvo == 0)
+                        {
+                            break;
+                        }
+
+                    }
+                }
             }
 
-            List<TrocoMoeda> troco = ConstruirTroco(moedas, dp);
             return troco;
         }
+
+
 
         static List<TrocoMoeda> CalcularTrocoDinamicoMoedas(List<double> moedas, decimal valor)
         {
             int valorCentavos = (int)(valor * 100);
             int[] dp = new int[valorCentavos + 1];
+            List<TrocoMoeda> troco = new List<TrocoMoeda>();
 
             for (int i = 1; i <= valorCentavos; i++)
             {
                 dp[i] = int.MaxValue;
-
                 foreach (var moeda in moedas)
                 {
                     int moedaCentavos = (int)(moeda * 100);
-
                     if (i >= moedaCentavos)
                     {
                         dp[i] = Math.Min(dp[i], 1 + dp[i - moedaCentavos]);
-                        
+                    }
+                }
+
+                if (dp[dp.Length - 1] != 0)
+                {
+                    int valorAlvo = i;
+                    int numeroMoedas = dp[dp.Length - 1];
+
+                    for (int j = 0; j < moedas.Count; j++)
+                    {
+                        int moedaAtual = (int)(moedas[j] * 100);
+                        int quantidade = Math.Min(numeroMoedas, valorAlvo / moedaAtual);
+
+                        if (quantidade > 0)
+                        {
+                            troco.Add(new TrocoMoeda(moedaAtual / 100.0, quantidade));
+                        }
+
+                        valorAlvo -= quantidade * moedaAtual;
+
+                        if (valorAlvo == 0)
+                        {
+                            break;
+                        }
                     }
                 }
             }
 
-            List<TrocoMoeda> troco = ConstruirTroco(moedas, dp);
             return troco;
         }
 
-        static List<TrocoMoeda> ConstruirTroco(List<double> moedas, int[] dp)
+//        static List<TrocoMoeda> ConstruirTroco(List<double> moedas, int[] dp)
+//{
+//    List<TrocoMoeda> troco = new List<TrocoMoeda>();
+//    int index = dp.Length - 1;
+
+//    while (index > 0)
+//    {
+//        //Console.WriteLine($"DEBUG: Entrou no loop principal. Index = {index}");
+
+//        foreach (var moeda in moedas)
+//        {
+//            int moedaInt = (int)moeda;
+//            int moedaCentavos = (int)(moeda * 100);
+
+//            //Console.WriteLine($"DEBUG: MoedaInt = {moedaInt}, MoedaCentavos = {moedaCentavos}");
+
+//            if (moedaInt > 0 && index >= moedaInt && dp[index] == 1 + dp[index - moedaInt])
+//            {
+//                int quantidade = 0;
+//                while (moedaInt > 0 && index >= moedaInt && dp[index] == 1 + dp[index - moedaInt])
+//                {
+//                    quantidade++;
+//                    index -= moedaInt;
+//                }
+
+//                //troco.Add(new TrocoMoeda(moedaInt, quantidade));
+//                // Console.WriteLine($"DEBUG: Adicionou troco - {quantidade} moeda(s) de {moedaInt}");
+//                break;
+//            }
+//            else if (moedaCentavos > 0 && index >= moedaCentavos && dp[index] == 1 + dp[index - moedaCentavos])
+//            {
+//                int quantidade = 0;
+//                while (moedaCentavos > 0 && index >= moedaCentavos && dp[index] == 1 + dp[index - moedaCentavos])
+//                {
+//                    quantidade++;
+//                    index -= moedaCentavos;
+//                }
+
+//                troco.Add(new TrocoMoeda(moeda, quantidade));
+//                //Console.WriteLine($"DEBUG: Adicionou troco - {quantidade} moeda(s) de {moeda}");
+//                break;
+//            }
+//        }
+//    }
+
+//    //Console.WriteLine("DEBUG: Saindo da função ConstruirTroco");
+//    return troco;
+//}
+
+
+static void MostrarTroco(List<TrocoMoeda> troco, string tipo)
+{
+    Console.WriteLine($"O troco em {tipo}s é: ");
+    foreach (var trocoMoeda in troco)
+    {
+        if (tipo == "moeda")
         {
-            List<TrocoMoeda> troco = new List<TrocoMoeda>();
-            int index = dp.Length - 1;
-
-            while (index > 0)
-            {
-                //Console.WriteLine($"DEBUG: Entrou no loop principal. Index = {index}");
-
-                foreach (var moeda in moedas)
-                {
-                    int moedaInt = (int)moeda;
-                    int moedaCentavos = (int)(moeda * 100);
-
-                    //Console.WriteLine($"DEBUG: MoedaInt = {moedaInt}, MoedaCentavos = {moedaCentavos}");
-
-                    if (moedaInt > 0 && index >= moedaInt && dp[index] == 1 + dp[index - moedaInt])
-                    {
-                        int quantidade = 0;
-                        while (moedaInt > 0 && index >= moedaInt && dp[index] == 1 + dp[index - moedaInt])
-                        {
-                            quantidade++;
-                            index -= moedaInt;
-                        }
-
-                        troco.Add(new TrocoMoeda(moedaInt, quantidade));
-                        // Console.WriteLine($"DEBUG: Adicionou troco - {quantidade} moeda(s) de {moedaInt}");
-                        break;
-                    }
-                    else if (moedaCentavos > 0 && index >= moedaCentavos && dp[index] == 1 + dp[index - moedaCentavos])
-                    {
-                        int quantidade = 0;
-                        while (moedaCentavos > 0 && index >= moedaCentavos && dp[index] == 1 + dp[index - moedaCentavos])
-                        {
-                            quantidade++;
-                            index -= moedaCentavos;
-                        }
-
-                        troco.Add(new TrocoMoeda(moeda, quantidade));
-                        //Console.WriteLine($"DEBUG: Adicionou troco - {quantidade} moeda(s) de {moeda}");
-                        break;
-                    }
-                }
-            }
-
-            //Console.WriteLine("DEBUG: Saindo da função ConstruirTroco");
-            return troco;
+            int valorMoeda = (int)(trocoMoeda.Moeda * 100);
+            Console.WriteLine($"{trocoMoeda.Quantidade} {tipo}s de {valorMoeda}");
         }
-
-
-        static void MostrarTroco(List<TrocoMoeda> troco, string tipo)
+        else
         {
-            Console.WriteLine($"O troco em {tipo}s é: ");
-            foreach (var trocoMoeda in troco)
-            {
-                if (tipo == "moeda")
-                {
-                    int valorMoeda = (int)(trocoMoeda.Moeda * 100);
-                    Console.WriteLine($"{trocoMoeda.Quantidade} {tipo}s de {valorMoeda}");
-                }
-                else
-                {
-                    Console.WriteLine($"{trocoMoeda.Quantidade} {tipo}s de {trocoMoeda.Moeda}");
-                }
-            }
+            Console.WriteLine($"{trocoMoeda.Quantidade} {tipo}s de {trocoMoeda.Moeda}");
         }
+    }
+}
 
         
     }
